9035a2cc4a0677a87a81808b9dc9835c7a0a7d31
Carsten Knoll <carsten.knoll@posteo.de>
2023-11-20 15:19:54 +0100

    stage_3c_create_file_names.py: major improvements

--- stage_3c_create_file_names.py
+++ stage_3c_create_file_names.py
@@ -6,6 +6,7 @@ import itertools as it
 import collections
 import argparse
 from colorama import Fore, Style
+from matplotlib import pyplot as plt
 
 import dill
 
@@ -36,7 +37,15 @@ parser = argparse.ArgumentParser(
 parser.add_argument(
     "--create-feature-dirs",
     "-cfd",
-    help="create direectories with file names containing feature values; e.g. cell_img_dir = critical_hist_chunk002",
+    help="create directories with file names containing feature values; e.g. cell_img_dir = critical_hist_chunk002",
+    metavar="cell_img_dir"
+)
+
+
+parser.add_argument(
+    "--create-stats",
+    "-cs",
+    help="create statistics on how the different features behave",
     metavar="cell_img_dir"
 )
 
@@ -44,9 +53,22 @@ parser.add_argument(
 args = parser.parse_args()
 
 
-def get_dirname(order):
-    dirname = "__".join(order)
+def get_dirname(keys):
+
+    prefixes = [PREFIXES[key] for key in keys]
+    dirname = "__".join(prefixes)
     return dirname
+
+
+# the keys should be kept in sync with the library (see comment `# anchor::db_keys`)
+
+PREFIXES = {
+    "crit_pix_nbr": "nbr",
+    "crit_pix_mean": "avg",
+    "crit_pix_median": "med",
+    "crit_pix_q95": "qnt",
+    "score_str": "A",
+}
 
 
 def create_feature_dirs():
@@ -56,19 +78,9 @@ def create_feature_dirs():
 
     assert fpaths, "unexpected empty list"
 
-    # the key should be kept in sync with the library (see comment `# anchor::db_keys`)
-
-    prefixes = {
-        "crit_pix_nbr": "nbr",
-        "crit_pix_mean": "avg",
-        "crit_pix_median": "med",
-        "crit_pix_q95": "qnt",
-        "score_str": "A",
-    }
-
     orders = [
         ("score_str", "crit_pix_nbr", "crit_pix_mean"),
-        ("crit_pix_nbr", "crit_pix_mean"),
+        ("crit_pix_nbr", "crit_pix_mean", "score_str"),
         ("crit_pix_mean", "crit_pix_nbr"),
         ("crit_pix_median", "crit_pix_nbr"),
         ("crit_pix_q95", "crit_pix_nbr"),
@@ -95,12 +107,14 @@ def create_feature_dirs():
                     assert len(arr) == 1
                     value = arr[0]
                 if isinstance(value, float):
-                    value = f"{value:.1f}"
+                    value = f"{value:05.1f}"
+                if isinstance(value, int):
+                    value = f"{value:04d}"
                 if value is None:
                     value = "0X"
 
-                assert isinstance(value, (str, int))
-                prefix = prefixes[key]
+                assert isinstance(value, str)
+                prefix = PREFIXES[key]
                 new_fname_parts.append(f"{prefix}{value}")
 
             new_fname = "_".join(new_fname_parts) + f"__{fname}"
@@ -114,11 +128,73 @@ def create_feature_dirs():
 
             # print("created:",  new_path)
 
+
+def create_stats():
+    basedir = args.create_stats
+    assert os.path.isdir(basedir)
+    fpaths = list(sorted(glob.glob(f"{basedir}/*.jpg")))
+
+    dirname = "_stats"
+    os.makedirs(os.path.join(basedir, dirname), exist_ok=True)
+
+    keys = list(PREFIXES.keys())
+
+    stat_combinations = [
+        ("crit_pix_nbr", "crit_pix_mean"),
+        ("crit_pix_nbr", "crit_pix_median"),
+        ("crit_pix_nbr", "crit_pix_q95"),
+        ("crit_pix_mean", "crit_pix_median"),
+        ("crit_pix_mean", "crit_pix_q95"),
+        ("crit_pix_mean", "score_str"),
+        ("crit_pix_nbr", "score_str"),
+    ]
+
+    num_data = collections.defaultdict(list)
+    for fpath in fpaths:
+        fname = os.path.split(fpath)[-1]
+        data = bs.db.get(fname)
+
+        if data is None:
+            print(f"could not find key {fname} in db")
+            continue
+
+        if data["crit_pix_nbr"] < 5:
+            # image is not interesting for stats
+            continue
+
+        for key in keys:
+            value = data[key]
+
+            if isinstance(value, str):
+                value = int(value)
+
+            num_data[key].append(value)
+
+    for x_key, y_key in stat_combinations:
+        xx = num_data[x_key]
+        yy = num_data[y_key]
+
+        xlabel = PREFIXES[x_key]
+        ylabel = PREFIXES[y_key]
+
+        plt.figure()
+        plt.plot(xx, yy, ".")
+        plt.xlabel(xlabel)
+        plt.ylabel(ylabel)
+        plt.title(f"{xlabel} vs {ylabel}")
+
+        fpath = os.path.join(basedir, dirname, f"{xlabel}_{ylabel}.png")
+
+        plt.savefig(fpath)
+
+
 def main():
 
     if args.create_feature_dirs:
         create_feature_dirs()
 
+    elif args.create_stats:
+        create_stats()
     else:
         parser.print_help()
 
