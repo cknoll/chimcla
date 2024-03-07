0c9c1d64f150ccdcdc7d544d983b91e200fa1333
Carsten Knoll <carsten.knoll@posteo.de>
2024-03-07 09:41:30 +0100

    add util scripts for folder-sorting and BGR-adjustment

---
+++ util_bgr_convert.py
@@ -0,0 +1,47 @@
+import os
+import sys
+import argparse
+
+import cv2
+
+from ipydex import IPS
+
+
+parser = argparse.ArgumentParser(
+    prog=sys.argv[0],
+    description='This program converts jpg files from BGR TO RGB and vice versa.',
+)
+
+
+parser.add_argument(
+    'dir',
+    help="directory",
+)
+
+
+def main():
+
+    args = parser.parse_args()
+    fnames = os.listdir(args.dir)
+
+    fnames.sort()
+
+    for fname in fnames:
+        if not fname.lower().endswith("jpg"):
+            continue
+        fpath = os.path.join(args.dir, fname)
+        img  = cv2.imread(fpath)
+        if img is None:
+            print(f"could not read {fpath}")
+            continue
+        try:
+            pass
+            res = cv2.imwrite(fpath, cv2.cvtColor(img, cv2.COLOR_RGB2BGR), [cv2.IMWRITE_JPEG_QUALITY, 100])
+        except Exception as e:
+            print(f"!! {e}")
+            continue
+        print(f"{fname} done")
+
+
+if __name__ == "__main__":
+    main()
---
+++ util_sort_into_folder.py
@@ -0,0 +1,51 @@
+import os
+import pandas as pd
+import numpy as np
+import glob
+
+# assumed working dir: where the images are, and where the dir `output` is
+
+
+data_path = "output/assigned_classes.csv"
+
+assert os.path.isfile(data_path)
+
+
+df = pd.read_csv(data_path)
+
+from ipydex import IPS
+
+dirs = np.array(["C1", "C2", "C3", "C4", "C5", "C6", "C7", "C8", "C9", "C0"])
+
+for d in dirs:
+    os.makedirs(d, exist_ok=True)
+
+
+
+processed_files = {}
+
+for index, row in df.iterrows():
+
+    mask = row[1:].to_numpy(dtype=bool)
+    cdirs = dirs[mask]
+    fname = row[0]
+    for cdir in cdirs:
+        cmd = f"cp {fname} {cdir}/{fname}"
+        os.system(cmd)
+
+    processed_files[fname] = 1
+
+    # break
+
+all_files = glob.glob("*.jpg")
+
+for fname in all_files:
+    if fname in processed_files:
+        continue
+    else:
+        cmd = f"cp {fname} C0/{fname}"
+        os.system(cmd)
+
+
+
+# IPS()
